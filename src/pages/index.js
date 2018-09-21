0c4e08f7e13de647225dd52e4b03818ff5d56afb
breadcrumbstudios <studio@breadcrumb.io>
2018-09-21 09:27:07 +0200

    updated font styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import print from "../images/skills/Print@2x.png"
 import web from "../images/skills/Web@2x.png"
 import dev from "../images/skills/Dev@2x.png"
 import hero from "../images/hero-bg.jpg"
+import arrow from "../images/arrow.png"
 
 const IndexPage = () => (
 	<div className="container">
@@ -66,24 +67,25 @@ const IndexPage = () => (
 
 		<section className="container work">
 			<div className="row section-title">
-				<p>Selected Experience</p>
+				<p>Selected Work Experience</p>
 			</div>
 			<div className="row work-list">
 
 				<div className="col-md-9 work-item">
 					<div className="row">
 						<div className="col-md-4">
-							<h4>On demand design services for tech start ups</h4>
+							<h4>On-demand design services for tech start ups</h4>
+							<p>Case study coming soon</p>
 						</div>
 						<div className="col-md-4">
 							<h5 className="extra-desc">Company:</h5>
 					   		<p className="extra-info">Breadcrumb Studio</p>
-					   		<h5 className="extra-desc">Duties:</h5>
-					   		<p className="extra-info">Management & Design Lead</p>
+					   		<h5 className="extra-desc">Role:</h5>
+					   		<p className="extra-info">Founder & Design Director</p>
 				   		</div>
 				   		<div className="col-md-4">
-							<h5 className="extra-desc">Role:</h5>
-					   		<p className="extra-info">Founder & Design Director</p>
+							<h5 className="extra-desc">Duties:</h5>
+					   		<p className="extra-info">Business & Project Management, Design Lead</p>
 					   		<h5 className="extra-desc">Timeframe:</h5>
 					   		<p className="extra-info">Oct 2014 - Jul 2018</p>
 				   		</div>
@@ -95,17 +97,18 @@ const IndexPage = () => (
 						<div className="col-md-4">
 							<Link to="resin">
 							<h4>Internet of Things software deployment done right.</h4>
+							<p>View Work<span className="arrow-icn"><img src={arrow} /></span></p>
 							</Link>
 						</div>
 						<div className="col-md-4">
 							<h5 className="extra-desc">Company:</h5>
 					   		<p className="extra-info">Resin.io</p>
-					   		<h5 className="extra-desc">Duties:</h5>
-					   		<p className="extra-info">Product & Marketing Design Lead</p>
+					   		<h5 className="extra-desc">Role:</h5>
+					   		<p className="extra-info">UI/UX Contractor</p>					   		
 				   		</div>
 				   		<div className="col-md-4">
-							<h5 className="extra-desc">Role:</h5>
-					   		<p className="extra-info">UI/UX Contractor</p>
+							<h5 className="extra-desc">Duties:</h5>
+					   		<p className="extra-info">Product & Marketing Design Lead</p>
 					   		<h5 className="extra-desc">Timeframe:</h5>
 					   		<p className="extra-info">Nov 2014 - Oct 2017</p>
 				   		</div>
@@ -116,16 +119,17 @@ const IndexPage = () => (
 					<div className="row">
 						<div className="col-md-4">
 							<h4>Media intelligence software maker.</h4>
+							<p>View Work<span className="arrow-icn"><img src={arrow} /></span></p>
 						</div>
 						<div className="col-md-4">
 							<h5 className="extra-desc">Company:</h5>
 					   		<p className="extra-info">Meltwater</p>
-					   		<h5 className="extra-desc">Duties:</h5>
-					   		<p className="extra-info">Design & Development</p>
+					   		<h5 className="extra-desc">Role:</h5>
+					   		<p className="extra-info">Digital Contractor</p>
 				   		</div>
 				   		<div className="col-md-4">
-							<h5 className="extra-desc">Role:</h5>
-					   		<p className="extra-info">Digital Contractor</p>
+					   		<h5 className="extra-desc">Duties:</h5>
+					   		<p className="extra-info">Marketing Design, WordPress Development</p>
 					   		<h5 className="extra-desc">Timeframe:</h5>
 					   		<p className="extra-info">Feb 2017 - Feb 2018</p>
 				   		</div>
@@ -136,16 +140,17 @@ const IndexPage = () => (
 					<div className="row">
 						<div className="col-md-4">
 							<h4>Automated legal spend management.</h4>
+							<p>View Work<span className="arrow-icn"><img src={arrow} /></span></p>
 						</div>
 						<div className="col-md-4">
 							<h5 className="extra-desc">Company:</h5>
 					   		<p className="extra-info">Apperio</p>
-					   		<h5 className="extra-desc">Duties:</h5>
-					   		<p className="extra-info">Design Lead</p>
-				   		</div>
-				   		<div className="col-md-4">
 							<h5 className="extra-desc">Role:</h5>
 					   		<p className="extra-info">Design Contractor</p>
+				   		</div>
+				   		<div className="col-md-4">
+					   		<h5 className="extra-desc">Duties:</h5>
+					   		<p className="extra-info">Brand Development, Product & Marketing Design</p>
 					   		<h5 className="extra-desc">Timeframe:</h5>
 					   		<p className="extra-info">Mar 2016 - Dec 2016</p>
 				   		</div>
@@ -154,6 +159,12 @@ const IndexPage = () => (
 				
 			</div>
 		</section>
+
+		<div className="container footer">
+			<div className="copyright">
+				<p>This website was designed by <Link to="about">Taahir Isaacs</Link> using Sketch & developed with Gatsby.js & Bootstrap 4. It took approximately 8 hours.</p>
+			</div>
+		</div>
 	</div>
 )
 
--- src/pages/resin.js
+++ src/pages/resin.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import resinlogo from "../images/resin-io.png"
+
+import res1 from "../images/case-study/res/res-1@2x.png"
+
 const Resin = () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  <div className="container">
+
+  	<section className="row section-one">
+	    <div className="col-md-12 work-item">
+					<div className="row">
+						<div className="col-md-6">
+							<h1>Internet of Things software deployment done right.</h1>
+						</div>
+						<div className="col-md-3">
+							<h5 className="extra-desc">Company:</h5>
+					   		<p className="extra-info">Resin.io</p>
+					   		<h5 className="extra-desc">Duties:</h5>
+					   		<p className="extra-info">Product & Marketing Design Lead</p>
+				   		</div>
+				   		<div className="col-md-3">
+							<h5 className="extra-desc">Role:</h5>
+					   		<p className="extra-info">UI/UX Contractor</p>
+					   		<h5 className="extra-desc">Timeframe:</h5>
+					   		<p className="extra-info">Nov 2014 - Oct 2017</p>
+				   		</div>
+			   		</div>
+				</div>
+    </section>
+
+    <section className="row">
+
+    	<div className="col-md-6">
+    	</div>
+
+    	<div className="col-md-6">
+    		<p>I was contracted to handle resin.io's internal and external design systems. The initial backlog of design work ranged from the redesign of their marketing website to calibrating all marketing materials with their updated branding. The key was to prioritise implementation of their new brand on touchpoints their customers would reach first then work my way out to seldomly viewed assets.</p>
+    	</div>
+    </section>
+    <section className="row">
+    	<div className="col-md-12">
+    		<img src={res1} />
+    	</div>
+    </section>
+
   </div>
 )
 
